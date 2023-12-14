02def691eb4f0f9b3767e1c2a7cf1511cb900efc
Sarah Otmane <sarahotmane@MacBook-Air-de-Sarah.local>
2023-12-14 16:14:02 +0100

    ajouter la fonction qui permet de verifier le token

---
+++ src/middlewares/jwtMidllewares.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+exports.verifyToken = async(req, res, next) =>{
+    try {
+        let token = req.headers['authorization'];
+        if(token != undefined){
+            
+            const payload = await new Promise((resolve, reject) =>{
+                jwt.verify(token, process.env.JWT_KEY, (error, decoded) =>{
+                    if(error){
+                        reject(error);
+                    }else{
+                        resolve(decoded);
+                    }
+                })
+            })
+
+            req.user = payload;
+            next();
+        }else{
+            res.status(403).json({message: "Accès interdit: token manquant"});
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(403).json({message: "Accès interdit: token invalide"});
+    }
+}
