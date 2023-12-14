edd04215b7a81bf771099a1949d33ef128b9c56a
Sarah Otmane <sarahotmane@MacBook-Air-de-Sarah.local>
2023-12-14 23:42:16 +0100

    ajouter la fonction hachage de mot de passe dans la fonction userModify dans le userController

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -71,9 +71,9 @@ exports.userModify = async(req, res) =>{
             req.user = payload;
 
             try{
+                req.body.password = await bcrypt.hash(req.body.password, 10);
                 const user = await User.findByIdAndUpdate(req.user.id, req.body, {new: true});
-                res.status(200);
-                res.json(user);
+                res.status(201).json({message : "votre mot de passe a bien été modifié"});
             }
             catch(error){
                 res.status(500);
