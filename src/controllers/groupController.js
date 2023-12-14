516a89e32297f97162f36e58743bd41d028ed38a
Sarah Otmane <sarahotmane@MacBook-Air-de-Sarah.local>
2023-12-14 23:03:10 +0100

    ajouter les deux fonction connectToAgroup qui permet de generer un token lors de la connection au groupe et deleteGroup qui permet a l admin du groupe de delete le groupe au groupeController ainsi qu au groupeRouter

--- src/controllers/groupController.js
+++ src/controllers/groupController.js
@@ -27,7 +27,7 @@ exports.createAGroup = async(req, res) =>{
 
                 let group = await newGroup.save();
 
-                res.status(201).json({message: `Groupe créer: ${group.name}`});
+                res.status(201).json({message: `Groupe créer: ${group.name} and ${group.id}`});
             }
             catch(error){
                 res.status(500);
@@ -41,5 +41,93 @@ exports.createAGroup = async(req, res) =>{
         res.status(500);
             console.log(error);
             res.json({ message : 'Erreur serveur'});
+    }
+}
+
+exports.connectToAGroup = async(req, res) =>{
+    try {
+        let token = req.headers['authorization'];
+        if(token != undefined){
+            const payload = await new Promise((resolve, reject) =>{
+                jwt.verify(token, process.env.JWT_KEY, (error, decoded) =>{
+                    if(error){
+                        reject(error);
+                    }else{
+                        resolve(decoded);
+                    }
+                })
+            })
+
+            req.user = payload;
+
+            try{
+                const group = await Group.findOne({_id:req.body.id, name: req.body.name});
+                if(!group){
+                    res.status(500).json({message: "Group non trouvé"});
+                    return;
+                }else{
+                    const groupData = {
+                        id: group._id,
+                        admin_id: group.admin_id,
+                        members_id: [group.members_id],
+                        user_id: req.user.id
+                    }
+                    
+                    const token= await jwt.sign(groupData, process.env.JWT_KEY, {expiresIn: '24h'});
+                    res.status(201).json(token);
+                }
+            }
+            catch(error){
+                res.status(500);
+                    console.log(error);
+                    res.json({ message : 'Erreur serveur lors du traitement'});
+            }
+        }else{
+            res.status(403).json({message: "Accès interdit: token manquant"});
+        } 
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message: 'Une erreur s\'est produite lors du traitement'});
+    }
+}
+
+
+exports.deleteGroup = async(req, res) =>{
+    try {
+        let token = req.headers['authorization'];
+        if(token != undefined){
+            const payload = await new Promise((resolve, reject) =>{
+                jwt.verify(token, process.env.JWT_KEY, (error, decoded) =>{
+                    if(error){
+                        reject(error);
+                    }else{
+                        resolve(decoded);
+                    }
+                })
+            })
+
+            req.group = payload;
+
+            if(req.group.admin_id == req.group.user_id){
+                try {
+                    const group = await Group.findByIdAndDelete(req.group.id, req.body, {new: true});
+                    res.status(201).json({message : 'Group Supprimé'});
+                    
+                } catch (error) {
+                    res.status(500);
+                        console.log(error);
+                        res.json({ message : 'Erreur serveur'});
+                }
+            }else{
+                res.status(403).json({message: "Accès interdit: Vous n\'etes pas l\'admin"});
+            }
+        }else{
+            res.status(403).json({message: "Accès interdit: token manquant"});
+        }  
+    } catch (error) {
+        res.status(500);
+            console.log(error);
+            res.json({ message : 'Erreur serveur (user inexistant)'});
     }
 }
--- src/routes/groupRoute.js
+++ src/routes/groupRoute.js
@@ -2,6 +2,94 @@ module.exports = (server) => {
     const groupController = require('../controllers/groupController');
     const jwtMiddleware = require('../middlewares/jwtMiddleware');
 
+/**
+ * @openapi
+ * tags:
+ *   name: Groups
+ *   description: CRUD of groups
+ */
 
-    server.post('/groups', jwtMiddleware.verifyToken, groupController.createAGroup);
+
+server.post('/groups/create', jwtMiddleware.verifyToken, groupController.createAGroup);
+/**
+ * @openapi
+ * /groups/create:
+ *   post:
+ *     summary: Create a new group
+ *     description: Endpoint to create a new group.
+ *     tags: [Groups]
+ *     security:
+ *       - BearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *             required:
+ *               - name
+ *               - id
+ *     responses:
+ *       201:
+ *         description: Group created successfully
+ *         content:
+ *           application/json:
+ *             example: { message: 'Group created successfully and its name is ${name}' }
+ */
+
+
+server.post('/groups/login', jwtMiddleware.verifyToken, groupController.connectToAGroup);
+/**
+ * @openapi
+ * /groups/login:
+ *   post:
+ *     summary: login to a group
+ *     description: Endpoint to login to a group.
+ *     tags: [Groups]
+ *     security:
+ *       - BearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               id:
+ *                 type: string
+ *             required:
+ *               - name
+ *               - id
+ *     responses:
+ *       201:
+ *         description: Group created successfully
+ *         content:
+ *           application/json:
+ *             example: { message: 'Group created successfully and its name is ${name}' }
+ */
+
+
+server.delete('/groups/admin', jwtMiddleware.verifyToken, groupController.deleteGroup);
+/**
+ * @openapi
+ * /groups/admin:
+ *   post:
+ *     summary: delete a group
+ *     description: Endpoint to delete a group.
+ *     tags: [Groups]
+ *     security:
+ *       - BearerAuth: []
+ *     responses:
+ *       201:
+ *         description: Group deleted successfully
+ *         content:
+ *           application/json:
+ *             example: { message: 'Group deleted successfully' }
+ */
+
 }
