f62cc1f238d85aaca7fd0e5757d5e3351109348b
Sarah Otmane <sarahotmane@MacBook-Air-de-Sarah.local>
2023-12-14 19:56:28 +0100

    création de la première version du groupModel

---
+++ src/models/groupModel.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let groupSchema = new Schema({
+    name:{
+        type : String,
+        required : true,
+    },
+    admin_id:{
+        type : String,
+        required : true
+    },
+    members_id:{
+        type: Array,
+        required : true
+    }
+});
+
+module.exports = mongoose.model('Group', groupSchema);
