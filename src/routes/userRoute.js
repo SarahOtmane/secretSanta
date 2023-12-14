acfc73ea2093628e95f7b30b634e965aefd2e0f5
Sarah Otmane <sarahotmane@MacBook-Air-de-Sarah.local>
2023-12-14 16:46:44 +0100

    ajouter le login, update et delete user dans le controller et dans le router

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,6 +1,5 @@
 module.exports = (server) => {
     const userController = require('../controllers/userController');
-    const jwtMiddleware = require('../middlewares/jwtMiddleware');
 
 /**
  * @openapi
@@ -38,4 +37,7 @@ module.exports = (server) => {
 
 
     server.post('/users/register', userController.userRegister);
+    server.post('/users/login', userController.loginRegister);
+    server.put('/users', jwtMiddleware.verifyToken, userController.userModify);
+    server.delete('/users', jwtMiddleware.verifyToken, userController.deleteUser);
 }
